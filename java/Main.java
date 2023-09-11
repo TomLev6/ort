0b2fc95f82248177cdb26e71f447493def14efe3
TomLev6 <100039158+TomLev6@users.noreply.github.com>
2023-09-11 11:44:11 +0300

    Add files via upload

---
+++ java/Main.java
@@ -0,0 +1,16 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+    public int similarname(Mother[] mothers, String kidName) {
+        int i,count=0;
+        for (i=0;i<mothers.length;i++){
+            if(mothers[i].hasKid(kidName)) count++;
+        }
+        return count;
+    }
+}
