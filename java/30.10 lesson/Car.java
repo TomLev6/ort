13d3502f757e3412d6f06b9901ca5e10aa453ae8
TomLev6 <100039158+TomLev6@users.noreply.github.com>
2023-10-30 12:13:43 +0200

    Add files via upload

---
+++ java/30.10 lesson/Car.java
@@ -0,0 +1,52 @@
+import java.util.Objects;
+
+public class Car
+{
+    protected String brand;
+    protected Integer year;
+    protected String lPlate;
+
+    public Car(String brand, Integer year, String lPlate) {
+        this.brand = brand;
+        this.year = year;
+        this.lPlate = lPlate;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getlPlate() {
+        return lPlate;
+    }
+
+    public void setlPlate(String lPlate) {
+        this.lPlate = lPlate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Objects.equals(brand, car.brand) && Objects.equals(year, car.year) && Objects.equals(lPlate, car.lPlate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(brand, year, lPlate);
+    }
+
+}
---
+++ java/30.10 lesson/Main.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Map<String, Car> map = new HashMap<>();
+
+        Car c1 = new Car("BMW",2018,"338-89-921");
+        Car c2 = new Car("PORSCHE",2009,"48-049-45");
+        Car c3 = new Car("KIA",2013,"51-079-40");
+
+        map.put("Tom",c1);
+        map.put("or",c2);
+        map.put("danny",c3);
+
+        for (String s: map.keySet()){
+            System.out.println(s);
+        }
+        for (Car c: map.values()){
+            System.out.println(c);
+        }
+        for (Map.Entry<String, Car> entry: map.entrySet()) {
+            System.out.println(entry);
+        }
+    }
+}
