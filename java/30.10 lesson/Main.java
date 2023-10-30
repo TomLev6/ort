951e2da41bb1330e5549b9b97f44ac47c73707b2
TomLev6 <100039158+TomLev6@users.noreply.github.com>
2023-10-30 12:49:31 +0200

    Update Main.java

--- java/30.10 lesson/Main.java
+++ java/30.10 lesson/Main.java
@@ -1,5 +1,4 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,6 +8,12 @@ public class Main {
         Car c1 = new Car("BMW",2018,"338-89-921");
         Car c2 = new Car("PORSCHE",2009,"48-049-45");
         Car c3 = new Car("KIA",2013,"51-079-40");
+
+        ArrayList<Car> arr = new ArrayList<>();
+        arr.add(c1);
+        arr.add(c2);
+        arr.add(c3);
+
 
         map.put("Tom",c1);
         map.put("or",c2);
@@ -23,5 +28,10 @@ public class Main {
         for (Map.Entry<String, Car> entry: map.entrySet()) {
             System.out.println(entry.getKey() + " drives " + entry.getValue().getBrand());
         }
+        System.out.println(arr);
+        System.out.println("\n");
+        Collections.sort(arr);
+        System.out.println(arr);
+
     }
 }
