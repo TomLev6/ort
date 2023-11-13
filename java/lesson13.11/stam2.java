2e26dc5db807507516ac696066e1a0f3c75fbd78
TomLev6 <100039158+TomLev6@users.noreply.github.com>
2023-11-13 14:29:37 +0200

    Add files via upload

---
+++ java/lesson13.11/stam2.java
@@ -0,0 +1,34 @@
+import java.lang.Math;
+
+public class stam2 extends Thread
+{
+    public static int sum;
+    public void run()
+    {
+        int min = 1;
+        int max = 10;
+        double a = Math.random() * (max - min + 1) + min;
+        sum+=(int)a;
+
+    }
+    public static void main(String[] args) throws InterruptedException {
+        sum = 0;
+        stam2 s = new stam2();
+        stam2 s1 = new stam2();
+        stam2 s2 = new stam2();
+        stam2 s3 = new stam2();
+        stam2 s4 = new stam2();
+        s.start();
+        s1.start();
+        s2.start();
+        s3.start();
+        s4.start();
+        s.join();
+        s1.join();
+        s2.join();
+        s3.join();
+        s4.join();
+        System.out.println(sum);
+
+    }
+}
