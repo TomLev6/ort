1f571a686d84e3cfc1abb9e76a1318d467fd05b7
TomLev6 <100039158+TomLev6@users.noreply.github.com>
2023-11-13 15:04:54 +0200

    Update stam2.java

--- java/lesson13.11/stam2.java
+++ java/lesson13.11/stam2.java
@@ -29,6 +29,28 @@ public class stam2 extends Thread
         s3.join();
         s4.join();
         System.out.println(sum);
-
+        /*
+         sum = 0;
+        stam2 s = new stam2();
+        stam2 s1 = new stam2();
+        stam2 s2 = new stam2();
+        stam2 s3 = new stam2();
+        stam2 s4 = new stam2();
+        List<stam2> list = new ArrayList<>();
+        list.add(s);
+        list.add(s1);
+        list.add(s2);
+        list.add(s3);
+        list.add(s4);
+        for (stam2 st: list)
+        {
+            st.start();
+        }
+        for (stam2 st: list)
+        {
+            st.join();
+        }
+        System.out.println(sum);
+        *\
     }
 }
