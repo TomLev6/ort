a9afaeea8709e35aebb2fc653b2be9e5ebc31776
TomLev6 <100039158+TomLev6@users.noreply.github.com>
2023-11-24 13:38:48 +0200

    Update test.c

--- c/Alma/23.11/test.c
+++ c/Alma/23.11/test.c
@@ -6,14 +6,14 @@
 
 void fun(Node** list, Node** res) {
 	Node* pos = *list;
-	Node* maxp= *list;
-	Node* minp= *list;
+	Node* maxp = *list;
+	Node* minp = *list;
 
 	Node* bmaxp = NULL;
 	Node* bminp = NULL;
 
-	int min= pos->info, max = pos->info;
-	while (pos->next!=NULL)
+	int min = pos->info, max = pos->info;
+	while (pos->next != NULL)
 	{
 		if (max < pos->next->info)
 		{
@@ -25,11 +25,11 @@ void fun(Node** list, Node** res) {
 			min = pos->next->info;
 			minp = pos->next;
 			bminp = pos;
-		}	
+		}
 		pos = pos->next;
 	}
 
-	if (bminp!=NULL)
+	if (bminp != NULL)
 	{
 		bminp->next = minp->next;
 	}
@@ -37,47 +37,60 @@ void fun(Node** list, Node** res) {
 	{
 		bmaxp->next = maxp->next;
 	}
-	if (maxp->info==(*list)->info)
+	if (maxp->info == (*list)->info)
 	{
 		//getting here!		
 		(*list) = (*list)->next;
-		
+
 	}
 	if (minp->info == (*list)->info)
 	{
 		(*list) = (*list)->next;
 	}
-	res = minp;
 	minp->next = maxp;
 	maxp->next = NULL;
+	*res = minp;
 
 	printList(*list);
 	printf("\n");
-	printList(res);
+	printList(*res);
 }
-
+void moreFun(Node* list, Node** res) {
+	Node* tmp;
+	int first = (*res)->info, last = (*res)->next->info;
+	int tmpVal = (first + last)/2;
+	getNode(&tmp, tmpVal);
+	tmp->next = (*res)->next;
+	(*res)->next = tmp;
+	tmpVal = first + last;
+	tmp->next->info = tmpVal;
+	tmpVal = first - last;
+	(*res)->info = tmpVal;
+	Node* pos = list;
+	while (pos->next!=NULL)
+	{
+		pos = pos->next;
+	}
+	pos->next = (*res);
+}
 int main() {
 	Node* n;
-	Node* res = NULL;
-	getNode(&n, 9);
+	Node* res=NULL;
+	getNode(&n, 6);
 	insertEnd(n, 4);
 	insertEnd(n, 1);
-	insertEnd(n, 8);
+	insertEnd(n, 5);
 	insertEnd(n, 3);
 	printList(n);
 	printf("\n");
 	fun(&n, &res);
-
-	//int num;
-	//Node* n;	
-	//scanf("%i", &num);
-	//getNode(n,&num);
-	//for (int i = 0; i < 2; i++) {
-	//	scanf("%i", &num);
-	//	insertEnd(n, num);
-	//	
-	//}
-
+	moreFun(n, &res);
+	printf("\n");
+	printList(n);
+	printf("\n");
+	printList(res);
 	
+
+
 	return 0;
 }
