90b1774959761486e701a803ba1fd7f98abbea6a
TomLev6 <100039158+TomLev6@users.noreply.github.com>
2024-01-25 09:36:52 +0200

    Add files via upload

---
+++ java/project /piece/Rook.java
@@ -0,0 +1,26 @@
+package piece;
+
+import main.GamePanel;
+
+public class Rook extends Piece{
+    public Rook(int color, int col, int row) {
+        super(color, col, row);
+        if (color== GamePanel.WHITE){
+            image = getImage("rook");
+        }
+        else  image = getImage("rook1");
+    }
+    public boolean canMove(int targetCol, int targetRow){
+        if (isWithinBoard(targetCol,targetRow)&&(!isSameSquare(targetCol,targetRow))){
+            // the formula to check if the rook move is valid
+            if( targetCol==preCol || targetRow==preRow)
+            {
+                if(isValidSquare(targetCol, targetRow)&&!pieceIsOnStraightLine(targetCol,targetRow)){
+                    return true;
+                }
+            }
+        }
+        return false;
+
+    }
+}
